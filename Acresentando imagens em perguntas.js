72c5fa036ef783840a84b8d437277506342f9864
Leonardo Kormann da Silva <leonardo.kormann@unifebe.edu.br>
2026-05-14 20:56:35 -0300

    Acresentando imagens em perguntas.js

--- Acresentando imagens em perguntas.js
+++ Acresentando imagens em perguntas.js
@@ -3,105 +3,105 @@ const perguntasOriginais = [
     pergunta: "BRUSQUE FICA EM QUAL ESTADO DO BRASIL?",
     opcoes: ["SANTA CATARINA", "PARANÁ", "BAHIA", "AMAZONAS"],
     correta: "SANTA CATARINA",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta1.png",
+    alt: "MAPA BRASIL"
   },
   {
     pergunta: "O QUE AS PESSOAS COSTUMAM USAR EM FESTAS TÍPICAS DE BRUSQUE?",
     opcoes: ["TRAJES TÍPICOS ALEMÃES", "ROUPA DE MERGULHO", "ARMADURA MEDIEVAL", "FANTASIA DE ASTRONAUTA"],
     correta: "TRAJES TÍPICOS ALEMÃES",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta2.png",
+    alt: "IMAGEM ROUPA"
   },
   {
     pergunta: "QUAL LUGAR DE BRUSQUE COSTUMA TER ÁRVORES, BANCOS E ESPAÇO PARA BRINCAR?",
     opcoes: ["PRAÇA", "FÁBRICA", "AEROPORTO", "GARAGEM"],
     correta: "PRAÇA",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta3.png",
+    alt: "IMAGEM ESPAÇO"
   },
   {
     pergunta: "QUAL COMIDA É BASTANTE CONHECIDA NA CULTURA ALEMÃ PRESENTE EM BRUSQUE?",
     opcoes: ["CUCA", "SUSHI", "TACO", "FEIJOADA"],
     correta: "CUCA",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta4.png",
+    alt: "IMAGEM COMIDA"
   },
   {
     pergunta: "BRUSQUE É CONHECIDA PRINCIPALMENTE POR QUAL ATIVIDADE?",
     opcoes: ["INDÚSTRIA TÊXTIL", "CRIAÇÃO DE FOGUETES", "PESCA NO OCEANO", "EXPLORAÇÃO ESPACIAL"],
     correta: "INDÚSTRIA TÊXTIL",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta5.png",
+    alt: "IMAGEM BRUSQUE"
   },
   {
     pergunta: "ONDE AS CRIANÇAS VÃO PARA APRENDER E ESTUDAR?",
     opcoes: ["ESCOLA", "CINEMA", "PADARIA", "MERCADO"],
     correta: "ESCOLA",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta6.png",
+    alt: "IMAGEM ESCOLA"
   },
   {
     pergunta: "QUAL MEIO DE TRANSPORTE É COMUM NAS RUAS DE BRUSQUE?",
     opcoes: ["ÔNIBUS", "SUBMARINO", "FOGUETE", "BALÃO MÁGICO"],
     correta: "ÔNIBUS",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta7.png",
+    alt: "IMAGEM TRANSPORTE"
   },
   {
     pergunta: "QUAL DESSES LUGARES AJUDA A CONTAR A HISTÓRIA DA CIDADE?",
     opcoes: ["MUSEU", "POSTO DE GASOLINA", "FARMÁCIA", "GARAGEM"],
     correta: "MUSEU",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta8.png",
+    alt: "IMAGEM LUGAR"
   },
   {
     pergunta: "O QUE DEVEMOS FAZER ANTES DE ATRAVESSAR A RUA?",
     opcoes: ["OLHAR PARA OS DOIS LADOS", "CORRER SEM OLHAR", "FECHAR OS OLHOS", "PULAR RAPIDAMENTE"],
     correta: "OLHAR PARA OS DOIS LADOS",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta9.png",
+    alt: "IMAGEM RUA"
   },
   {
     pergunta: "QUAL RIO PASSA PELA CIDADE DE BRUSQUE?",
     opcoes: ["RIO ITAJAÍ-MIRIM", "RIO AMAZONAS", "RIO SÃO FRANCISCO", "RIO NILO"],
     correta: "RIO ITAJAÍ-MIRIM",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta10.png",
+    alt: "IMAGEM RIO"
   },
   {
     pergunta: "QUAL DESSES LUGARES É USADO PARA CUIDAR DA SAÚDE DAS PESSOAS?",
     opcoes: ["HOSPITAL", "TEATRO", "SHOPPING", "BIBLIOTECA"],
     correta: "HOSPITAL",
-    imagem:"imagens/hospital.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta11.png",
+    alt: "IMAGEM HOSPITAL"
   },
   {
     pergunta: "EM BRUSQUE, ONDE AS PESSOAS PODEM COMPRAR ROUPAS E TECIDOS?",
     opcoes: ["LOJAS E SHOPPINGS", "CASTELOS", "PIRÂMIDES", "ESTAÇÕES ESPACIAIS"],
     correta: "LOJAS E SHOPPINGS",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta12.png",
+    alt: "IMAGEM LOJAS"
   },
   {
     pergunta: "QUAL DESSES ANIMAIS PODE SER ENCONTRADO EM PARQUES E ÁREAS VERDES DE BRUSQUE?",
     opcoes: ["PÁSSAROS", "CAMELOS", "PINGUINS GIGANTES", "DRAGÕES"],
     correta: "PÁSSAROS",
-    imagem:"imagens/ponte.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta13.png",
+    alt: "IMAGEM ANIMAIS"
   },
   {
     pergunta: "O QUE AJUDA A MANTER BRUSQUE LIMPA E BONITA?",
     opcoes: ["JOGAR LIXO NA LIXEIRA", "JOGAR PAPEL NO CHÃO", "QUEBRAR PLACAS", "PICHAR PAREDES"],
     correta: "JOGAR LIXO NA LIXEIRA",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta14.png",
+    alt: "IMAGEM LIXO"
   },
   {
-    pergunta: "QUAL DESSES OPÇÕES REPRESENTA ALGO CULTURAL DE BRUSQUE?",
+    pergunta: "QUAL DESSAS OPÇÕES REPRESENTA ALGO CULTURAL DE BRUSQUE?",
     opcoes: ["FESTAS TÍPICAS E TRADIÇÃO ALEMÃ", "PIRÂMIDES ANTIGAS", "VULCÕES ATIVOS", "TEMPLOS SUBMARINOS"],
     correta: "FESTAS TÍPICAS E TRADIÇÃO ALEMÃ",
-    imagem:"Festas.jpg",
-    alt:"imagem festa"
+    imagem: "imagens/pergunta15.png",
+    alt: "IMAGEM FESTA"
   }
 ];
